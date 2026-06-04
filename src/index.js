a405c8357c16f6b77603841d51594cfb91d7d9e7
Hongildonn <temuulen.amazing@gmail.com>
2026-06-04 15:52:29 +0900

    Create basic JavaScript starter template in src/index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * Main Application Entry Point
+ * 
+ * This is a basic JavaScript starter template.
+ * Modify this file to add your application logic.
+ */
+
+// Example: Simple function
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Example: Execute function
+console.log(greet('World'));
+
+// Example: Export for use in other modules (if using ES modules)
+// export { greet };
+
+// Example: CommonJS export (if using Node.js)
+// module.exports = { greet };
